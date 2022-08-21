f40945ba5aff66389a98e5ad63f0b5738729da98
SirBlobman <blobmansrealm@gmail.com>
2022-08-21 09:25:51 -0400

    2.5.0: Update to BlueSlimeCore 2.5.

--- src/main/java/com/github/sirblobman/respawn/RespawnPlugin.java
+++ src/main/java/com/github/sirblobman/respawn/RespawnPlugin.java
@@ -2,8 +2,11 @@ package com.github.sirblobman.respawn;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import com.github.sirblobman.api.configuration.ConfigurationManager;
+import com.github.sirblobman.api.bstats.bukkit.Metrics;
+import com.github.sirblobman.api.bstats.charts.SimplePie;
 import com.github.sirblobman.api.core.CorePlugin;
+import com.github.sirblobman.api.language.Language;
+import com.github.sirblobman.api.language.LanguageManager;
 import com.github.sirblobman.api.plugin.ConfigurablePlugin;
 import com.github.sirblobman.api.update.UpdateManager;
 import com.github.sirblobman.respawn.command.CommandRespawnX;
@@ -12,22 +15,45 @@ import com.github.sirblobman.respawn.listener.ListenerRespawnX;
 public final class RespawnPlugin extends ConfigurablePlugin {
     @Override
     public void onLoad() {
-        ConfigurationManager configurationManager = getConfigurationManager();
-        configurationManager.saveDefault("config.yml");
+        saveDefaultConfig();
     }
 
     @Override
     public void onEnable() {
-        new CommandRespawnX(this).register();
-        new ListenerRespawnX(this).register();
-
-        CorePlugin corePlugin = JavaPlugin.getPlugin(CorePlugin.class);
-        UpdateManager updateManager = corePlugin.getUpdateManager();
-        updateManager.addResource(this, 47058L);
+        reloadConfig();
+        registerCommands();
+        registerListeners();
+        registerUpdateChecker();
+        registerbStats();
     }
 
     @Override
     public void onDisable() {
         // Do Nothing
+    }
+
+    private void registerCommands() {
+        new CommandRespawnX(this).register();
+    }
+
+    private void registerListeners() {
+        new ListenerRespawnX(this).register();
+    }
+
+    private void registerUpdateChecker() {
+        CorePlugin corePlugin = JavaPlugin.getPlugin(CorePlugin.class);
+        UpdateManager updateManager = corePlugin.getUpdateManager();
+        updateManager.addResource(this, 47058L);
+    }
+
+    private void registerbStats() {
+        Metrics metrics = new Metrics(this, 16217);
+        metrics.addCustomChart(new SimplePie("selected_language", this::getDefaultLanguageCode));
+    }
+
+    private String getDefaultLanguageCode() {
+        LanguageManager languageManager = getLanguageManager();
+        Language defaultLanguage = languageManager.getDefaultLanguage();
+        return (defaultLanguage == null ? "none" : defaultLanguage.getLanguageCode());
     }
 }
--- src/main/java/com/github/sirblobman/respawn/command/CommandRespawnX.java
+++ src/main/java/com/github/sirblobman/respawn/command/CommandRespawnX.java
@@ -5,17 +5,15 @@ import java.util.List;
 import java.util.Locale;
 
 import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import com.github.sirblobman.api.command.Command;
 import com.github.sirblobman.api.utility.MessageUtility;
 import com.github.sirblobman.respawn.RespawnPlugin;
 
 public final class CommandRespawnX extends Command {
-    private final RespawnPlugin plugin;
-
     public CommandRespawnX(RespawnPlugin plugin) {
         super(plugin, "respawnx");
-        this.plugin = plugin;
     }
 
     @Override
@@ -38,7 +36,9 @@ public final class CommandRespawnX extends Command {
             return false;
         }
 
-        this.plugin.reloadConfig();
+        JavaPlugin plugin = getPlugin();
+        plugin.reloadConfig();
+
         String message = MessageUtility.color("&2[RespawnX]&a Successfully reloaded the configuration file.");
         sender.sendMessage(message);
         return true;
